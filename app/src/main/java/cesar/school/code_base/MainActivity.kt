4b8fe10f2e3246025bec96ba14ce88a428d284b6
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-01-09 17:24:21 -0300

    view binding

--- app/src/main/java/cesar/school/code_base/MainActivity.kt
+++ app/src/main/java/cesar/school/code_base/MainActivity.kt
@@ -2,10 +2,20 @@ package cesar.school.code_base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import cesar.school.code_base.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding // var to later initiate the binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // why need to inflate?
+        setContentView(binding.root)  // changed from R.layout.activity_main to binding.root, why?
+
+        binding.buttonViewBinding.setOnClickListener {
+            Toast.makeText(this, this.getString(R.string.button_view_binding_working), Toast.LENGTH_SHORT).show()
+        }
     }
 }
